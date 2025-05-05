f52f34460c1ca6cce127113c12f5808872417af7
MathLight-c <65977110+MathLight-c@users.noreply.github.com>
2025-05-05 11:58:08 +0200

    FirstLoop Gameplay

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -3,7 +3,31 @@ local item_tints = require("__base__.prototypes.item-tints")
 local space_age_item_sounds = require("__space-age__.prototypes.item_sounds")
 
 data:extend({
+<<<<<<< Updated upstream
 
+=======
+  -- Item Subgroup for defining Sorting Group in the Player Inventory
+  {
+    type = "item-subgroup",
+    name = "water-production",
+    group = "production",
+    order = "ea"
+  },
+
+  -- Tier 1 Water Condenser Machine Item Definition
+  {
+    type = "item",
+    name = "water-condenser",
+    icon = "__arrakis_my_dune__/graphics/icons/sand-1.png",
+    subgroup = "smelting-machine",
+    order = "ffj",
+    inventory_move_sound = item_sounds.mechanical_large_inventory_move,
+    pick_sound = item_sounds.mechanical_large_inventory_pickup,
+    drop_sound = item_sounds.mechanical_large_inventory_move,
+    place_result = "water-condenser",
+    stack_size = 20,
+  },
+>>>>>>> Stashed changes
 
 --- subgroups
   {
@@ -38,5 +62,157 @@ data:extend({
     default_import_location = "arrakis2",
     random_tint_color = item_tints.iron_rust,
     weight = 1*kg,
+  },
+  {
+    type = "item",
+    name = "silica",
+    icon = "__arrakis_my_dune__/graphics/icons/silica.png",
+    subgroup = "arrakis2-processes",
+    order = "aab",
+    icon_size = 64,
+    pictures =
+    {
+      { size = 64, filename = "__arrakis_my_dune__/graphics/icons/silica.png", scale = 0.5 },
+    },
+    stack_size = 100,
+    default_import_location = "arrakis2",
+    random_tint_color = item_tints.iron_rust,
+    weight = 1*kg,
+  },
+  {
+    type = "item",
+    name = "silicon",
+    icon = "__arrakis_my_dune__/graphics/icons/silicon.png",
+    subgroup = "arrakis2-processes",
+    order = "aab",
+    icon_size = 64,
+    pictures =
+    {
+      { size = 64, filename = "__arrakis_my_dune__/graphics/icons/silicon.png", scale = 0.5 },
+    },
+    stack_size = 100,
+    default_import_location = "arrakis2",
+    random_tint_color = item_tints.iron_rust,
+    weight = 1*kg,
+  },
+  {
+    type = "item",
+    name = "silicon-paste",
+    icon = "__arrakis_my_dune__/graphics/icons/silicon-paste.png",
+    subgroup = "arrakis2-processes",
+    order = "aab",
+    icon_size = 64,
+    pictures =
+    {
+      { size = 64, filename = "__arrakis_my_dune__/graphics/icons/silicon-paste.png", scale = 0.5 },
+    },
+    stack_size = 100,
+    default_import_location = "arrakis2",
+    random_tint_color = item_tints.iron_rust,
+    weight = 1*kg,
+  },
+  {
+    type = "item",
+    name = "treated-sand",
+    icon = "__arrakis_my_dune__/graphics/icons/treated-sand.png",
+    subgroup = "arrakis2-processes",
+    order = "aac",
+    icon_size = 64,
+    pictures =
+    {
+      { size = 64, filename = "__arrakis_my_dune__/graphics/icons/treated-sand.png", scale = 0.5 },
+    },
+    stack_size = 100,
+    default_import_location = "arrakis2",
+    random_tint_color = item_tints.iron_rust,
+    weight = 1*kg,
+  },
+  {
+    type = "item",
+    name = "metallic-sand",
+    icon = "__arrakis_my_dune__/graphics/icons/metallic-sand.png",
+    subgroup = "arrakis2-processes",
+    order = "aac",
+    icon_size = 64,
+    pictures =
+    {
+      { size = 64, filename = "__arrakis_my_dune__/graphics/icons/metallic-sand.png", scale = 0.5 },
+    },
+    stack_size = 100,
+    default_import_location = "arrakis2",
+    random_tint_color = item_tints.iron_rust,
+    weight = 1*kg,
+  },
+  {
+    type = "item",
+    name = "mineral-sand",
+    icon = "__arrakis_my_dune__/graphics/icons/mineral-sand.png",
+    subgroup = "arrakis2-processes",
+    order = "aac",
+    icon_size = 64,
+    pictures =
+    {
+      { size = 64, filename = "__arrakis_my_dune__/graphics/icons/mineral-sand.png", scale = 0.5 },
+    },
+    stack_size = 100,
+    default_import_location = "arrakis2",
+    random_tint_color = item_tints.iron_rust,
+    weight = 1*kg,
+  },
+  {
+    type = "item",
+    name = "wafers",
+    icon = "__arrakis_my_dune__/graphics/icons/wafers.png",
+    subgroup = "arrakis2-processes",
+    order = "aac",
+    icon_size = 64,
+    pictures =
+    {
+      { size = 64, filename = "__arrakis_my_dune__/graphics/icons/wafers.png", scale = 0.5 },
+    },
+    stack_size = 100,
+    default_import_location = "arrakis2",
+    random_tint_color = item_tints.iron_rust,
+    weight = 0.1*kg,
+  },
+  {
+    type = "item",
+    name = "silicon-filter",
+    icon = "__arrakis_my_dune__/graphics/icons/silicon-filter.png",
+    subgroup = "arrakis2-processes",
+    order = "aac",
+    icon_size = 64,
+    pictures =
+    {
+      { size = 64, filename = "__arrakis_my_dune__/graphics/icons/silicon-filter.png", scale = 0.5 },
+    },
+    stack_size = 100,
+    default_import_location = "arrakis2",
+    random_tint_color = item_tints.iron_rust,
+    weight = 0.1*kg,
+  },
+  {
+    type = "fluid",
+    name = "chemical-mud",
+    subgroup = "arrakis2-processes",
+    default_temperature = 15,
+    max_temperature = 100,
+    heat_capacity = "2kJ",
+    base_color = {0.35, 0.15, 0.05},
+    flow_color = {0.6, 0.3, 0.1},
+    icon = "__arrakis_my_dune__/graphics/icons/fluid/chemical-mud.png",
+    order = "a[fluid]-a[water]-a[water]"
+  },
+  {
+    type = "fluid",
+    name = "polluted-water",
+    subgroup = "arrakis2-processes",
+    default_temperature = 15,
+    max_temperature = 100,
+    heat_capacity = "2kJ",
+    base_color = {0.25, 0.3, 0.1},
+    flow_color = {0.5, 0.55, 0.2},
+    icon = "__arrakis_my_dune__/graphics/icons/fluid/polluted-water.png",
+    order = "a[fluid]-a[water]-a[water]"
   },
 })
--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -1,4 +1,19 @@
 data:extend({
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   {
     type = "recipe",
     name = "concrete-from-molten-iron-and-sand",
@@ -44,4 +59,364 @@ data:extend({
     auto_recycle = false,
     allow_productivity = true
   },
+<<<<<<< Updated upstream
 })
+=======
+
+
+
+
+  --WATER CONDENSING RECIPES CATEGORY
+  {
+    type = "recipe-category",
+    name = "water-condensing"
+  },
+
+  --WATER CONDENSING RECIPE
+  {
+    type = "recipe",
+    name = "atmospheric-water",
+    category = "water-condensing",
+    energy_required = 10,
+    ingredients = nil,
+    results = {{type = "fluid", name = "water", amount = 10}},
+    main_product = "water",
+    allow_productivity = false,
+    allow_quality = false,
+  },
+  {
+    type = "recipe",
+    name = "sand-extraction",
+    category = "water-condensing",
+    energy_required = 10,
+    ingredients = nil,
+    results = {{type = "item", name = "sand", amount = 100}},
+    main_product = "sand",
+    allow_productivity = false,
+    allow_quality = false,
+  },
+
+  --------------SAND TREATMENT--------------
+
+  {
+    type = "recipe",
+    name = "sand-whashing",
+    category = "chemistry",
+    energy_required = 10,
+    enabled = true,
+    auto_recycle = false,
+    ingredients =
+    {
+      {type = "fluid", name = "water", amount = 100},
+      {type = "item", name = "sand", amount = 100}
+    },
+    results =
+    {
+      {type = "fluid", name = "chemical-mud", amount = 80},
+      {type = "item", name = "treated-sand", amount = 20}
+    },
+    main_product = "treated-sand",
+    allow_productivity = true,
+    crafting_machine_tint =
+    {
+      primary = {r = 1.000, g = 0.886, b = 0.678, a = 1.000}, -- #ffe2adff
+      secondary = {r = 0.992, g = 0.733, b = 0.408, a = 1.000}, -- #fdbb68ff
+      tertiary = {r = 0.945, g = 0.804, b = 0.580, a = 1.000}, -- #f1cda3ff
+      quaternary = {r = 1.000, g = 0.945, b = 0.753, a = 1.000}, -- #fff1c0ff
+    }
+  },
+  {
+    type = "recipe",
+    name = "sandTreatment",
+    category = "chemistry",
+    energy_required = 0.5,
+    enabled = true,
+    auto_recycle = false,
+    ingredients =
+    {
+      {type = "item", name = "treated-sand", amount = 1}
+    },
+    results =
+    {
+      {type = "item", name = "metallic-sand", amount = 1, probability = 0.6},
+      {type = "item", name = "mineral-sand", amount = 1, probability = 0.4}
+    },
+    main_product = "mineral-sand",
+    allow_productivity = true,
+    crafting_machine_tint =
+    {
+      primary = {r = 1.000, g = 0.886, b = 0.678, a = 1.000}, -- #ffe2adff
+      secondary = {r = 0.992, g = 0.733, b = 0.408, a = 1.000}, -- #fdbb68ff
+      tertiary = {r = 0.945, g = 0.804, b = 0.580, a = 1.000}, -- #f1cda3ff
+      quaternary = {r = 1.000, g = 0.945, b = 0.753, a = 1.000}, -- #fff1c0ff
+    }
+  },
+  {
+    type = "recipe",
+    name = "metallic-sandTreatment1",
+    category = "chemistry",
+    icon = "__arrakis_my_dune__/graphics/icons/metallic-sandTreatment.png",
+    energy_required = 1,
+    enabled = true,
+    auto_recycle = false,
+    ingredients =
+    {
+      {type = "item", name = "metallic-sand", amount = 1}
+    },
+    results =
+    {
+      {type = "item", name = "iron-ore", amount = 1, probability = 0.5},
+      {type = "item", name = "copper-ore", amount = 1, probability = 0.5}
+    },
+    main_product = "iron-ore",
+    allow_productivity = true,
+    crafting_machine_tint =
+    {
+      primary = {r = 1.000, g = 0.886, b = 0.678, a = 1.000}, -- #ffe2adff
+      secondary = {r = 0.992, g = 0.733, b = 0.408, a = 1.000}, -- #fdbb68ff
+      tertiary = {r = 0.945, g = 0.804, b = 0.580, a = 1.000}, -- #f1cda3ff
+      quaternary = {r = 1.000, g = 0.945, b = 0.753, a = 1.000}, -- #fff1c0ff
+    }
+  },
+  {
+    type = "recipe",
+    name = "metallic-sandTreatment2",
+    category = "chemistry",
+    icon = "__arrakis_my_dune__/graphics/icons/metallic-sandTreatment2.png",
+    energy_required = 20,
+    enabled = true,
+    auto_recycle = false,
+    ingredients =
+    {
+      {type = "item", name = "metallic-sand", amount = 20}
+    },
+    results =
+    {
+      {type = "item", name = "iron-ore", amount = 8},
+      {type = "item", name = "copper-ore", amount = 8},
+      {type = "item", name = "tungsten-ore", amount = 4}
+    },
+    main_product = "iron-ore",
+    allow_productivity = true,
+    crafting_machine_tint =
+    {
+      primary = {r = 1.000, g = 0.886, b = 0.678, a = 1.000}, -- #ffe2adff
+      secondary = {r = 0.992, g = 0.733, b = 0.408, a = 1.000}, -- #fdbb68ff
+      tertiary = {r = 0.945, g = 0.804, b = 0.580, a = 1.000}, -- #f1cda3ff
+      quaternary = {r = 1.000, g = 0.945, b = 0.753, a = 1.000}, -- #fff1c0ff
+    }
+  },
+  {
+    type = "recipe",
+    name = "mineral-sandTreatment1",
+    category = "chemistry",
+    energy_required = 1,
+    enabled = true,
+    auto_recycle = false,
+    ingredients =
+    {
+      {type = "item", name = "mineral-sand", amount = 1}
+    },
+    results =
+    {
+      {type = "item", name = "stone", amount = 1, probability = 0.7},
+      {type = "item", name = "silica", amount = 1, probability = 0.25},
+      {type = "item", name = "calcite", amount = 1, probability = 0.05}
+    },
+    main_product = "silica",
+    allow_productivity = true,
+    crafting_machine_tint =
+    {
+      primary = {r = 1.000, g = 0.886, b = 0.678, a = 1.000}, -- #ffe2adff
+      secondary = {r = 0.992, g = 0.733, b = 0.408, a = 1.000}, -- #fdbb68ff
+      tertiary = {r = 0.945, g = 0.804, b = 0.580, a = 1.000}, -- #f1cda3ff
+      quaternary = {r = 1.000, g = 0.945, b = 0.753, a = 1.000}, -- #fff1c0ff
+    }
+  },
+  {
+    type = "recipe",
+    name = "mineral-sandTreatment2",
+    category = "chemistry",
+    energy_required = 20,
+    enabled = true,
+    auto_recycle = false,
+    ingredients =
+    {
+      {type = "item", name = "mineral-sand", amount = 20}
+    },
+    results =
+    {
+      {type = "item", name = "stone", amount = 13},
+      {type = "item", name = "silica", amount = 4},
+      {type = "item", name = "holmium-ore", amount = 2},
+      {type = "item", name = "calcite", amount = 1}
+    },
+    main_product = "silica",
+    allow_productivity = true,
+    crafting_machine_tint =
+    {
+      primary = {r = 1.000, g = 0.886, b = 0.678, a = 1.000}, -- #ffe2adff
+      secondary = {r = 0.992, g = 0.733, b = 0.408, a = 1.000}, -- #fdbb68ff
+      tertiary = {r = 0.945, g = 0.804, b = 0.580, a = 1.000}, -- #f1cda3ff
+      quaternary = {r = 1.000, g = 0.945, b = 0.753, a = 1.000}, -- #fff1c0ff
+    }
+  },
+
+  --------------FLUID TREATMENT--------------
+
+  {
+    type = "recipe",
+    name = "chemical-mudTreatment1",
+    category = "chemistry",
+    icon = "__arrakis_my_dune__/graphics/icons/chemical-mudTreatment1.png",
+    energy_required = 10,
+    enabled = true,
+    auto_recycle = false,
+    ingredients =
+    {
+      {type = "fluid", name = "chemical-mud", amount = 100}
+    },
+    results =
+    {
+      {type = "item", name = "coal", amount = 10},
+      {type = "fluid", name = "polluted-water", amount = 90}
+      
+    },
+    main_product = "polluted-water",
+    allow_productivity = true,
+    crafting_machine_tint =
+    {
+      primary = {r = 1.000, g = 0.886, b = 0.678, a = 1.000}, -- #ffe2adff
+      secondary = {r = 0.992, g = 0.733, b = 0.408, a = 1.000}, -- #fdbb68ff
+      tertiary = {r = 0.945, g = 0.804, b = 0.580, a = 1.000}, -- #f1cda3ff
+      quaternary = {r = 1.000, g = 0.945, b = 0.753, a = 1.000}, -- #fff1c0ff
+    }
+  },
+  {
+    type = "recipe",
+    name = "chemical-mudTreatment2",
+    category = "chemistry",
+    icon = "__arrakis_my_dune__/graphics/icons/chemical-mudTreatment1.png",
+    energy_required = 10,
+    enabled = true,
+    auto_recycle = false,
+    ingredients =
+    {
+      {type = "fluid", name = "chemical-mud", amount = 100}
+    },
+    results =
+    {
+      {type = "item", name = "spoilage", amount = 10},
+      {type = "fluid", name = "sulfuric-acid", amount = 40},
+      {type = "fluid", name = "polluted-water", amount = 50}
+    },
+    main_product = "polluted-water",
+    allow_productivity = true,
+    crafting_machine_tint =
+    {
+      primary = {r = 1.000, g = 0.886, b = 0.678, a = 1.000}, -- #ffe2adff
+      secondary = {r = 0.992, g = 0.733, b = 0.408, a = 1.000}, -- #fdbb68ff
+      tertiary = {r = 0.945, g = 0.804, b = 0.580, a = 1.000}, -- #f1cda3ff
+      quaternary = {r = 1.000, g = 0.945, b = 0.753, a = 1.000}, -- #fff1c0ff
+    }
+  },
+  {
+    type = "recipe",
+    name = "polluted-waterTreatment",
+    category = "oil-processing",
+    enabled = true,
+    energy_required = 10,
+    ingredients =
+    {
+      {type = "item", name = "silicon-filter", amount = 1},
+      {type = "fluid", name = "polluted-water", amount = 100, ignored_by_stats = 25}
+    },
+    results =
+    {
+      {type = "fluid", name = "water", amount = 100, ignored_by_stats = 25, ignored_by_productivity = 25}
+    },
+    allow_productivity = true,
+    main_product = "water",
+    subgroup = "fluid-recipes",
+    order = "a[oil-processing]-c[coal-liquefaction]",
+    allow_decomposition = false
+  },
+  --------------SILICON FILTERS--------------
+  {
+    type = "recipe",
+    name = "silicon-paste",
+    enabled = true,
+    energy_required = 5,
+    ingredients = 
+    {
+      {type = "item", name = "silica", amount = 1},
+      {type = "item", name = "carbon", amount = 1}
+    },
+    results = 
+    {
+      {type="item", name="silicon-paste", amount=1}
+    },
+    main_product = "silicon-paste",
+    allow_productivity = true
+  },
+  {
+    type = "recipe",
+    name = "silicon",
+    category = "smelting",
+    enabled = true,
+    energy_required = 5,
+    ingredients = 
+    {
+      {type = "item", name = "silicon-paste", amount = 1}
+      
+    },
+    results = 
+    {
+      {type="item", name="silicon", amount=1}
+    },
+    main_product = "silicon",
+    allow_productivity = true
+  },
+  {
+    type = "recipe",
+    name = "silicon-filter",
+    category = "crafting",
+    energy_required = 1,
+    enabled = true,
+    auto_recycle = false,
+    ingredients =
+    {
+      {type = "item", name = "silicon", amount = 1},
+      {type = "item", name = "plastic-bar", amount = 1}
+    },
+    results =
+    {
+      {type = "item", name = "silicon-filter", amount = 1}
+    },
+    main_product = "silicon-filter",
+    allow_productivity = true,
+  },
+
+  --WATER CONDENSER MACHINE RECIPE
+  {
+        type = "recipe",
+        name = "water-condenser",
+        category = "basic-crafting",
+        energy_required = 2,   
+        ingredients = {
+            { type = "item", name = "iron-gear-wheel", amount = 5 },
+            { type = "item", name = "electronic-circuit", amount = 3 },
+            { type = "item", name = "steel-plate", amount = 2 },
+        },
+        results = {
+          { type = "item", name = "water-condenser", amount = 1 },
+        },
+        allow_productivity = false,
+        allow_quality = false,
+  },
+  
+
+
+
+})
+>>>>>>> Stashed changes
