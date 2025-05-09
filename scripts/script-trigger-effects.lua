c5a0343d8b02fd53f51c68c17a922a85685a01fd
Schaniqua <68185180+Schaniqua@users.noreply.github.com>
2025-05-09 16:22:46 +0200

    Fix & removed lightning
    Lightning moisture generation removed. Timespan added to couple moisture generation to nighttime. Can be adjusted in loop.lua
    
    Coupled dew collectors to nighttime, will only produce liquid at night.
    
    Set both dew-collector & water-condenser as fixed-recipe and adjusted magic numbers
    
    TODO:
    Adjust magic numbers for humidity absorption, add magic numbers to arrakis terrain tiles, add worm targeting per get_random_chunk()

--- scripts/script-trigger-effects.lua
+++ scripts/script-trigger-effects.lua
@@ -1,8 +1,11 @@
-local POLLUTION_PER_LIGHTNING_STRIKE = 5
+-- PLACEHOLDER, NOT WORKING ANYMORE
+
+-- Pollution added to chunk per singular lightning strike
+local POLLUTION_PER_LIGHTNING_STRIKE = 0.1
 
 
 
-
+-- Script-Trigger when lightning hits something this gets called and executed
 script.on_event("on_script_trigger_effect", function(event)
     if (event.effect_id == "pollution_trigger") then
         game.surfaces["arrakis"].pollute(event.target_position, POLLUTION_PER_LIGHTNING_STRIKE, humidity)
