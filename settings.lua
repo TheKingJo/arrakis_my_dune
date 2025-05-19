2f41af3e402ad229dc61e7c85e4d4d12922faed2
Schaniqua <68185180+Schaniqua@users.noreply.github.com>
2025-05-20 00:18:18 +0200

    Startup Settings for devolopment
    New startup settings

--- settings.lua
+++ settings.lua
@@ -3,12 +3,24 @@ data:extend{
         type = "bool-setting",
         name = "hide-default-aai-vehicles",
         setting_type = "startup",
-        default_value = true,
+        default_value = true
     },
     {
         type = "bool-setting",
         name = "enable_aai_miners_planet_skin",
         setting_type = "startup",
-        default_value = true,
+        default_value = true
+    },
+    {
+        type = "int-setting",
+        name = "worm_attacks_frequency",
+        setting_type = "startup",
+        default_value = 200
+    },
+    {
+        type = "int-setting",
+        name = "worm_attacks_damage",
+        setting_type = "startup",
+        default_value = 999999
     }
 }
